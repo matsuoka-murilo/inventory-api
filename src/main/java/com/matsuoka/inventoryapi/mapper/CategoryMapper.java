56765e1248ea2aa40c3d7089cc72e6cacb830143
Murilo Matsuoka <murilo.matsuoka@gmail.com>
2025-11-22 13:45:19 -0300

    feat: add CategoryMapper and ProductMapper

---
+++ src/main/java/com/matsuoka/inventoryapi/mapper/CategoryMapper.java
@@ -0,0 +1,10 @@
+package com.matsuoka.inventoryapi.mapper;
+
+import com.matsuoka.inventoryapi.domain.Category;
+import com.matsuoka.inventoryapi.dto.CategoryDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public abstract class CategoryMapper {
+    public abstract Category toCategory(CategoryDTO categoryDTO);
+}
--- src/main/java/com/matsuoka/inventoryapi/mapper/ProductMapper.java
+++ src/main/java/com/matsuoka/inventoryapi/mapper/ProductMapper.java
@@ -1,3 +1,10 @@
 package com.matsuoka.inventoryapi.mapper;
+
+import com.matsuoka.inventoryapi.domain.Product;
+import com.matsuoka.inventoryapi.dto.ProductDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public abstract class ProductMapper {
+    public abstract Product toProduct(ProductDTO productDTO);
 }
