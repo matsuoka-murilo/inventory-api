15fe73c7b7a8db7328df1223e14bc2e42b57ffba
Murilo Matsuoka <murilo.matsuoka@gmail.com>
2025-11-22 16:39:42 -0300

    feat: add fixed categories with data.sql

---
+++ data.sql
@@ -0,0 +1,4 @@
+INSERT INTO category (name) VALUES ('Eletrônicos');
+INSERT INTO category (name) VALUES ('Móveis');
+INSERT INTO category (name) VALUES ('Alimentos');
+INSERT INTO category (name) VALUES ('Roupas');
