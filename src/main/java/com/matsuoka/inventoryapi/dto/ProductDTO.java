2a5d3afbc6179a33ca094a801d23a6bb1114f3a7
Murilo Matsuoka <murilo.matsuoka@gmail.com>
2025-11-22 11:52:11 -0300

    feat: add response DTOs for Product and Category

--- src/main/java/com/matsuoka/inventoryapi/dto/ProductDTO.java
+++ src/main/java/com/matsuoka/inventoryapi/dto/ProductDTO.java
@@ -1,6 +1,52 @@
 package com.matsuoka.inventoryapi.dto;
 
 public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Long category_id;
 
+    public ProductDTO() {
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Long getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(Long category_id) {
+        this.category_id = category_id;
+    }
 }
