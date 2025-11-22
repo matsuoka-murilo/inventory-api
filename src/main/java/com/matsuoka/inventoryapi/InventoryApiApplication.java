3622ab45d1a75f3bfbc397744833f3970e08ae9e
Murilo Matsuoka <murilo.matsuoka@gmail.com>
2025-11-22 11:08:07 -0300

    Initial commit - Spring Boot project

---
+++ src/main/java/com/matsuoka/inventoryapi/InventoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.matsuoka.inventoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryApiApplication.class, args);
+	}
+
+}
