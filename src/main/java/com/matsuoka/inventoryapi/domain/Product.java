75e89b1d52ae71a42c7c0060f60e30e009d30a0e
Murilo Matsuoka <murilo.matsuoka@gmail.com>
2025-11-22 11:37:15 -0300

    feat: add Product end Category Entity

---
+++ src/main/java/com/matsuoka/inventoryapi/domain/Product.java
@@ -0,0 +1,56 @@
+package com.matsuoka.inventoryapi.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    @ManyToOne
+    private Category category;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, Category category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
